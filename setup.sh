891256961d9d119abe953627a1d3ac36ef4998d1
Renato <renato.srdantas@gmail.com>
2019-02-24 14:08:00 -0300

    add wights of vgg16 in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,22 @@
+# download dog images
 wget https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/dogImages.zip
 
 unzip dogImages.zip
 
 rm -f dogImages.zip
 
+
+# downloand humam images
 wget https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/lfw.zip
 
 unzip lfw.zip
 
 rm -f lfw.zip
 
+
+# download vgg16 wights
+cd bottleneck_features
+
+wget https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/DogVGG16Data.npz
+
+cd ..
