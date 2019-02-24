0c7da2507311c135c3f960320de49ce8c234b559
Renato <renato.srdantas@gmail.com>
2019-02-24 13:49:14 -0300

    create a setup file for download datasets

---
+++ setup.sh
@@ -0,0 +1,12 @@
+wget https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/dogImages.zip
+
+unzip dogImages.zip
+
+rm -f dogImages.zip
+
+wget https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/lfw.zip
+
+unzip lfw.zip
+
+rm -f lfw.zip
+
